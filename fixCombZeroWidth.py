6b0118b55a1849092accbe420b0061a2e20e96ce
petrvanblokland <buro@petr.com>
2024-05-29 10:35:06 +0200

    Fix Comb Zero Width

---
+++ fixCombZeroWidth.py
@@ -0,0 +1,16 @@
+for f in AllFonts():
+    for g in f:
+        if 'comb' in g.name:
+            if 'pnum' in g.name:
+                if round(g.angledLeftMargin) or round(g.angledRightMargin):
+                    print(g.name, g.width, f.path)
+                    g.angledLeftMargin = 0
+                    g.angledRightMargin = 0
+                    g.changed()
+            elif g.width != 0:
+                print(g.name, g.width, f.path)
+                g.width = 0
+                g.changed()
+            
+            
+print('Done')
