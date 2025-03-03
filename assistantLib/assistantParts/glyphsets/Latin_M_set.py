5a1d7b4873f03d51c7c742b761d84b30fa02c5eb
petrvanblokland <buro@petr.com>
2025-03-03 10:12:52 +0100

    Anchor rules

--- assistantLib/assistantParts/glyphsets/Latin_M_set.py
+++ assistantLib/assistantParts/glyphsets/Latin_M_set.py
@@ -208,7 +208,7 @@ GDS['brevebelow'] = GD(name='brevebelow', uni=0x032E, hex='032E', c='̮', w=0, b
 
 # d
 
-GDS['dlinebelow'] = GD(name='dlinebelow', uni=0x1E0F, hex='1E0F', c='ḏ', base='d', accents=['macronbelowcmb'], isLower=True, anchors=['bottom', 'middle', 'top'], gid=938)
+GDS['dlinebelow'] = GD(name='dlinebelow', uni=0x1E0F, hex='1E0F', c='ḏ', base='d', rightMin='-100', anchorTopX='TopX', anchorTopY='TopY', accents=['macronbelowcmb'], isLower=True, anchors=['bottom', 'middle', 'top'], gid=938)
 GDS['dong'] = GD(name='dong', uni=0x20AB, hex='20AB', c='₫', base='d', l=GD.CAT_CENTER, w=GD.CAT_MATH_WIDTH, isLower=True, gid=1444, comment='₫ vietnamese currency')
 
 # e
